ded1926cb24944e2a705b35eddf395d8352bb8d9
Eduard <eduard.ryazapov@gmail.com>
2017-07-05 17:51:50 +0300

    setup devise

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,11 @@ module Calendar
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    
+
     # Set default From address for all Mailers
     config.action_mailer.default_options = { from: ENV.fetch("MAILER_SENDER_ADDRESS") }
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Set URL options to be able to use url_for helpers
+    config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
   end
 end
