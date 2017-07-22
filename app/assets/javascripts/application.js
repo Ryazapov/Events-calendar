d129ab8465a7723c40fb4201acc4eb2715075937
Eduard <eduard.ryazapov@gmail.com>
2017-07-22 17:52:20 +0300

    config foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require foundation
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(function(){ $(document).foundation(); });
