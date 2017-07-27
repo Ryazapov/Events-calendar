7954ff10e1dff342777fd24959fc228367127393
Eduard <eduard.ryazapov@gmail.com>
2017-07-27 15:05:47 +0300

    setup pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  include Authorization
+
+    protect_from_forgery with: :exception
+
+    responders :flash
+    respond_to :html
 end
