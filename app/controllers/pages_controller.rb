9d243c81724b6d4fc91b925f8d43dff200c22584
Eduard <eduard.ryazapov@gmail.com>
2017-08-15 02:39:02 +0300

    change simple calendar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,31 @@
 class PagesController < ApplicationController
   def index
+    return unless current_user
+    @start_date = start_date
+    calendar_service = CalendarService.new(current_month)
+    @events = calendar_service.events
+    @event_types_by_day = calendar_service.event_types_by_day
+  end
+
+  private
+
+  def start_date
+    if params[:start_date]
+      date = parse_start_date
+      return date.at_beginning_of_month if date
+    end
+
+    date = Time.zone.today.at_beginning_of_month
+    params[:start_date] = date
+  end
+
+  def parse_start_date
+    Date.parse(params[:start_date])
+  rescue ArgumentError
+    nil
+  end
+
+  def current_month
+    @start_date..@start_date.end_of_month
   end
 end
