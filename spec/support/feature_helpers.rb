da0dbff696b55d23a2a74a2180e5216082b4d663
Eduard <eduard.ryazapov@gmail.com>
2017-08-16 02:27:43 +0300

    rubocop fix

--- spec/support/feature_helpers.rb
+++
@@ -1,9 +0,0 @@
-# RSpec.configure do |config|
-#   config.define_derived_metadata(file_path: %r{/spec/system/}) do |metadata|
-#     metadata[:type] = :feature
-#   end
-#
-#   config.with_options(type: :feature) do |feature|
-#     feature.include PageHelpers
-#   end
-# end
