13854731d1ef6f6ed5705cb0f08b57291cbaa8c3
Eduard <eduard.ryazapov@gmail.com>
2017-08-21 01:21:57 +0300

    fix rubocop and delete unused method

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -2,10 +2,6 @@ class EventDecorator < ApplicationDecorator
   delegate_all
 
   def date
-    object.date.strftime("%d/%m/%Y") if object.date
-  end
-
-  def day_of_date
-    object.date.day if object.date
+    object.date&.strftime("%d/%m/%Y")
   end
 end
