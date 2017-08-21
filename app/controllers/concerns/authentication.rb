106284c24cafc0f4ec94d0f6211d101b574c5c5a
Eduard <eduard.ryazapov@gmail.com>
2017-08-21 11:27:27 +0300

    use concern authentication

---
+++ app/controllers/concerns/authentication.rb
@@ -0,0 +1,12 @@
+module Authentication
+  extend ActiveSupport::Concern
+  private
+
+  def devise_parameter_sanitizer
+    if resource_class == User
+      User::ParameterSanitizer.new(User, :user, params)
+    else
+      super
+    end
+  end
+end
